c078bed94575b1402ef557d0f538aa5278356fd5
Eric Li <ericli@bitgo.com>
2025-04-10 13:11:45 -0400

    feat(core): cache circuit compilation
    BTC-0

--- core/main.go
+++ core/main.go
@@ -3,7 +3,7 @@ package main
 import "bitgo.com/proof_of_reserves/circuit"
 
 func main() {
-	batchCount := 3
+	batchCount := 10
 	GenerateData(batchCount)
 	Prove(batchCount)
 	account := readDataFromFile[circuit.GoAccount]("out/secret/test_account.json")
