4ae116264ed52aa446f847facf038899b330c60b
Eric Li <ericli@bitgo.com>
2025-04-10 11:25:18 -0400

    refactor: read account from data
    BTC-0

--- core/main.go
+++ core/main.go
@@ -1,10 +1,15 @@
 package main
 
+import "bitgo.com/proof_of_reserves/circuit"
+
 func main() {
 	batchCount := 3
 	GenerateData(batchCount)
 	Prove(batchCount)
-	proofElement := getDataFromFiles[ProofElements](1, "out/secret/test_data_")
-	Verify(batchCount, proofElement[0].Accounts[0])
+	account, err := readDataFromFile[circuit.GoAccount]("out/secret/test_account.json")
+	if err != nil {
+		panic(err)
+	}
+	Verify(batchCount, account)
 	print("Proof succeeded!")
 }
--- core/utils.go
+++ core/utils.go
@@ -59,6 +59,7 @@ type CompletedProof struct {
 }
 
 func writeTestDataToFile(batchCount int, countPerBatch int) {
+	var lastAccount *circuit.GoAccount
 	for i := 0; i < batchCount; i++ {
 		filePath := "out/secret/test_data_" + strconv.Itoa(i) + ".json"
 		var secretData ProofElements
@@ -70,17 +71,19 @@ func writeTestDataToFile(batchCount int, countPerBatch int) {
 			panic(err)
 		}
 
-		//filePath = "../out/public/test_data_" + string(rune(i)) + ".json"
-		//var publicData PublicProofElements
-		//_, _, publicData.MerkleRoot, publicData.MerkleRootWithAssetSumHash = circuit.GenerateTestData(countPerBatch)
-		//err = writeJson(filePath, publicData)
-		//if err != nil {
-		//	panic(err)
-		//}
+		lastAccount = &secretData.Accounts[0]
+	}
+
+	if lastAccount == nil {
+		panic("lastAccount is nil")
+	}
+	err := writeJson("out/secret/test_account.json", lastAccount)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func readDataFromFile[D ProofElements | CompletedProof](filePath string) (D, error) {
+func readDataFromFile[D ProofElements | CompletedProof | circuit.GoAccount](filePath string) (D, error) {
 	var data D
 	err := readJson(filePath, &data)
 	if err != nil {
